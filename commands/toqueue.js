3447826064fe94c30582b914c06b16f4589f1bec
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-05-29 13:40:41 +1000

    initial commit

---
+++ commands/toqueue.js
@@ -0,0 +1,26 @@
+const { Command } = require('smooth-discord.js');
+const { stripIndents } = require('common-tags');
+
+module.exports = class QueueCommand extends Command {
+	constructor() {
+		super({
+			name: 'toqueue',
+			description: 'Moves all users with the "Team Captains" role to the queue room.',
+			guildOnly: true,
+			ownerOnly: true
+		});
+	}
+
+	run(message, args) {
+		const teamCaptainRole = message.guild.roles.get('300601294706442241');
+		const queueRoom = message.guild.channels.get('163508200589623298');
+		const membersToMove = message.guild.members.filter(member => member.roles.has(teamCaptainRole) && member.voiceChannel);
+
+		membersToMove.map(mem => mem.setVoiceChannel(queueRoom));
+
+		message.reply(stripIndents`
+			Moved these users to the queue room.
+			
+			⊳ ${membersToMove.map(mem => mem.displayName).join('\n⊳ ')}`);
+	}
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+const Smooth = require('smooth-discord.js');
+const path = require('path');
+const { token } = require('./config.json');
+const client = new Smooth.Client({
+	// eslint-disable-next-line
+	// owners: ['135991380760592384', '142858948058284032', '135991380760592384', '189696688657530880']
+	owners: ['189696688657530880'],
+	prefix: '-',
+	commandDirectory: path.join(__dirname, 'commands'),
+	debug: true,
+	unkownCommandResponse: true,
+	errorResponse: true
+});
+
+client.login(token);
