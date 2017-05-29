75f268d2384e5778d35391050d9a609e2ec2dd4c
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-05-29 14:24:23 +1000

    Add toRoom command

---
+++ commands/torooms.js
@@ -0,0 +1,31 @@
+const { Command } = require('smooth-discord.js');
+const { stripIndents } = require('common-tags');
+
+module.exports = class RoomCommand extends Command {
+	constructor() {
+		super({
+			name: 'toroom',
+			description: 'Moves all users with the "Team Captains" role back to their team chats.',
+			guildOnly: true,
+			ownerOnly: true
+		});
+	}
+
+	run(message, args) {
+		const teamCaptainRole = message.guild.roles.get('300601294706442241');
+		const queueRoom = message.guild.channels.get('163508200589623298');
+		const adminRole = message.guild.roles.get('292271962544275456');
+		const membersToMove = queueRoom.members.filter(member => member.roles.has(teamCaptainRole));
+		const voiceChannels = message.guild.channels.filter(channel => channel.type === 'voice');
+
+		membersToMove.map(mem => {
+			const teamRole = mem.roles.filter(role => role !== adminRole || role.id !== message.member.guild.id).first();
+			mem.setVoiceChannel(voiceChannels.find('name', teamRole.name));
+		});
+
+		message.reply(stripIndents`
+			Moved these users back to their team rooms.
+			
+			⊳ ${membersToMove.map(mem => mem.displayName).join('\n⊳ ')}`);
+	}
+};
