8ab30175517b061a57b32bb8489f4684d0c1219c
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-05-29 15:41:33 +1000

    update

--- commands/torooms.js
+++ commands/torooms.js
@@ -20,7 +20,9 @@ module.exports = class RoomCommand extends Command {
 
 		membersToMove.forEach(mem => {
 			const teamRole = mem.roles.filter(role => role !== adminRole || role.id !== message.member.guild.id).first();
-			mem.setVoiceChannel(voiceChannels.find('name', teamRole.name));
+			const voiceChannel = voiceChannels.find('name', teamRole.name);
+			console.log(teamRole.name);
+			console.log(voiceChannel.name);
 		});
 
 		message.reply(stripIndents`
