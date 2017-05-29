07aa45f4d4f5bb36a12ea3e994b3f3031306da34
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-05-29 22:13:36 +1000

    .

--- commands/captain.js
+++ commands/captain.js
@@ -13,14 +13,14 @@ module.exports = class CaptainCommand extends Command {
 
 	run(message, args) {
 		const member = findMember(message, args);
-		console.log(member);
+
 		if (member === null) {
 			error('Please input a valid user', message);
 		} else {
 			const roleToGive = message.guild.roles.get('300601294706442241');
 
 			member.addRole(roleToGive).then(mem => {
-				message.reply(`**${member.displayName}** has been given the **${roleToGive.name}** for 6 hours. This will be removed automatically.\n*unless i break during that time :')*`);
+				message.reply(`**${member.displayName}** has been given the **${roleToGive.name}** role for 6 hours. This will be removed automatically.\n*unless i break during that time :')*`);
 				setTimeout(() => {
 					mem.removeRole(roleToGive);
 				}, 10000);
