349a8331cfe8c4f98b78536ac8c5f9c505df502e
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-06-01 15:02:39 +1000

    .

--- commands/torooms.js
+++ commands/torooms.js
@@ -16,12 +16,13 @@ module.exports = class RoomCommand extends Command {
 		const adminRole = message.guild.roles.get('292271962544275456');
 		const membersToMove = queueRoom.members.filter(member => member.voiceChannel);
 
-		if (membersToMove.size === 0) return message.reply('There\'s no team members in any channels?');
+		if (membersToMove.size === 0) return message.reply('Can\'t find any team members in voice channels.');
 
 		const voiceChannels = message.guild.channels.filter(channel => channel.type === 'voice');
 
 		membersToMove.forEach(mem => {
 			const teamRole = mem.roles.filter(role => role !== adminRole && role.id !== message.member.guild.id && role.id !== '300601294706442241').first();
+			console.log(teamRole.name);
 			if (!teamRole) return;
 			const voiceChannel = voiceChannels.find('name', teamRole.name);
 			mem.setVoiceChannel(voiceChannel);
