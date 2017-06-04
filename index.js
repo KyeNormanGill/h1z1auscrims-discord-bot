7500c996b9bd20372131fe7910205e51ba266da1
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-06-04 15:04:09 +1000

    logs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Smooth = require('smooth-discord.js');
 const path = require('path');
 const { token } = require('./config.json');
+const { stripIndents } = require('common-tags');
+const moment = require('moment');
+
 const client = new Smooth.Client({
 	// eslint-disable-next-line
 	owners: ['135991380760592384', '142858948058284032', '135991380760592384', '189696688657530880'],
@@ -29,4 +32,22 @@ client.on('ready', () => {
 		client.user.setGame(notifs[gameTick]);
 		if (++gameTick === notifs.length) gameTick = 0;
 	}, 300000);
+});
+
+client.on('guildMemberAdd', member => {
+	client.channels.get('320788665863241728').send(stripIndents`
+		:white_check_mark: 
+		The user **${member.user.username}** has joined the discord.
+
+		**${moment().format('h:mm a, Do MMMM YYYY')}**
+	`);
+});
+
+client.on('guildMemberRemove', member => {
+	client.channels.get('320788665863241728').send(stripIndents`
+		:negative_squared_cross_mark: 
+		The user **${member.user.username}** has left the discord.
+
+		**${moment().format('h:mm a, Do MMMM YYYY')}**
+	`);
 });
