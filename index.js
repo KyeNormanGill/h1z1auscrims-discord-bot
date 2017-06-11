dd4fad54b000046aede8c15483aa06adf9d4bcbd
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-06-11 17:23:57 +1000

    .

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Smooth = require('smooth-discord.js');
 const path = require('path');
 const { token } = require('./config.json');
-const { stripIndents } = require('common-tags');
-const moment = require('moment');
 
 const client = new Smooth.Client({
 	owners: ['135991380760592384', '142858948058284032', '135991380760592384', '189696688657530880'],
@@ -16,24 +14,6 @@ client.login(token);
 
 client.on('ready', () => {
 	client.user.setGame('5s with the boys!');
-});
-
-client.on('guildMemberAdd', member => {
-	client.channels.get('320788665863241728').send(stripIndents`
-		:white_check_mark: 
-		The user **${member.user.username}** has joined the discord.
-
-		**${moment().format('h:mm a, Do MMMM YYYY')}**
-	`);
-});
-
-client.on('guildMemberRemove', member => {
-	client.channels.get('320788665863241728').send(stripIndents`
-		:negative_squared_cross_mark: 
-		The user **${member.user.username}** has left the discord.
-
-		**${moment().format('h:mm a, Do MMMM YYYY')}**
-	`);
 });
 
 client.on('presenceUpdate', (oldM, newM) => require('./events/presenceUpdate.js').handleEvent(oldM, newM));
