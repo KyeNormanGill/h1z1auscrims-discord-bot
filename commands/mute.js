7f4c3e1370ee7cf814803137929e620b3d173cdd
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-07-15 19:50:56 +1000

    :l

--- commands/mute.js
+++ commands/mute.js
@@ -14,7 +14,7 @@ module.exports = class LockdownCommand extends Command {
 		if (!message.member.roles.has('322794011133870080') || !message.member.roles.has('292271962544275456')) return;
 		if (message.channel.id !== '334258750200217601' || message.channel.id !== '334258848523354122' || message.channel.id !== '324578223142993931') return message.chanel.send('Only the general chats and LFT chat can be muted.');
 
-		message.channel.overwritePermissions(message.guild.roles.get(message.guild.id), { 'SEND_MESSAGES': false }).then(() => {
+		message.channel.overwritePermissions(message.guild.roles.get(message.guild.id), { SEND_MESSAGES: false }).then(() => {
 			message.channel.send(`**${message.channel.name}** is now muted! Use \`-unmute\` to unmute the chat. `);
 		});
 	}
