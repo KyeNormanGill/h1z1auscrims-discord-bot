6a177e44ab94a3a409811649ed8c858ed30af47a
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-07-15 19:49:13 +1000

    big update :>

---
+++ commands/mute.js
@@ -0,0 +1,21 @@
+const { Command } = require('smooth-discord.js');
+
+module.exports = class LockdownCommand extends Command {
+	constructor() {
+		super({
+			name: 'mute',
+			description: 'Lockdown the discord.',
+			guildOnly: true,
+			aliases: ['lock']
+		});
+	}
+
+	run(message) {
+		if (!message.member.roles.has('322794011133870080') || !message.member.roles.has('292271962544275456')) return;
+		if (message.channel.id !== '334258750200217601' || message.channel.id !== '334258848523354122' || message.channel.id !== '324578223142993931') return message.chanel.send('Only the general chats and LFT chat can be muted.');
+
+		message.channel.overwritePermissions(message.guild.roles.get(message.guild.id), { 'SEND_MESSAGES': false }).then(() => {
+			message.channel.send(`**${message.channel.name}** is now muted! Use \`-unmute\` to unmute the chat. `);
+		});
+	}
+};
