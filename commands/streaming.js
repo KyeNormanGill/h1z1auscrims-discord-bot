20b594fe2f53e1ad54555dc7fbe7466dda416c33
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-07-20 19:10:32 +1000

    .

--- commands/streaming.js
+++ commands/streaming.js
@@ -26,7 +26,8 @@ module.exports = class StreamingCommand extends Command {
 			const streamID = mem.user.presence.game.url.split('/').slice(3).join();
 			const url = `https://api.twitch.tv/kraken/streams/${streamID}?client_id=${twitch}`;
 			snekfetch.get(url).then(res => {
-				console.log(res.body.stream.game);
+				console.log(res.body.stream);
+				//console.log(res.body.stream.game);
 				text += `**${mem.displayName}** - ${res.body.stream.game} - <${res.body.stream.channel.url}>\n`;
 			}).then(() => {
 				count++;
