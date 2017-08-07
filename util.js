3356d198ef054b91954c71bb33c1acad31d3dc76
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-08-07 17:44:09 +1000

    .

--- util.js
+++ util.js
@@ -76,7 +76,7 @@ function updateStreaming(client) {
 					}
 					console.log(`Updating live: ${new Date()}`);
 
-					message.edit(`${div1}\n${div1MultiLink ? `*Multilink ${div1MultiLink}*` : ''}\n\n${div2}\n${div2MultiLink ? `*Multilink ${div2MultiLink}*` : ''}`).catch(console.error);
+					message.edit(`${div1}\n${div1MultiLink ? `**Multilink:** ${div1MultiLink}` : ''}\n\n${div2}\n${div2MultiLink ? `**Multilink:** ${div2MultiLink}` : ''}`).catch(console.error);
 				});
 			});
 		});
