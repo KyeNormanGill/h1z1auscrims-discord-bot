e055e8520ce6ce8d2362886c48818d421c1f951a
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-08-07 14:48:21 +1000

    .

--- commands/streaming.js
+++ commands/streaming.js
@@ -16,7 +16,7 @@ module.exports = class StreamingCommand extends Command {
 		let text = `Users streaming **H1Z1: KOTK** in **${message.guild.name}**\n\n`;
 		let count = 0;
 		let max = 0;
-		let names = [];
+		const names = [];
 
 		const users = message.guild.members.filter(member => member.user.presence.game && member.user.presence.game.streaming);
 		max = users.size;
@@ -27,6 +27,8 @@ module.exports = class StreamingCommand extends Command {
 			const streamID = mem.user.presence.game.url.split('/').slice(3).join();
 			const url = `https://api.twitch.tv/kraken/streams/${streamID}?client_id=${twitch}`;
 			snekfetch.get(url).then(res => {
+				console.log(url);
+				console.log(res.body);
 				if (!res.body.stream) return;
 				if (res.body.stream.game !== 'H1Z1: King of the Kill') return;
 				names.push(`${streamID}/`);
@@ -37,11 +39,9 @@ module.exports = class StreamingCommand extends Command {
 
 				if (count === max) {
 					if (names.length > 1) {
-						console.log(`${names.length} is greater than 1`);
 						snekfetch.get(`http://tinyurl.com/api-create.php?url=http://multitwitch.tv/${names.join('')}`)
 							.then(res => {
 								text += `\nWatch them all at: ${res.text}`;
-								console.log(res.text);
 								message.channel.send(text);
 							});
 					} else {
