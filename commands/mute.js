028fe040ec1b62679c91e8dd7df966f39a50b05c
KyeNormanGill <kyenormangill@tafensw.net.au>
2017-12-04 17:26:10 +1100

    testing

--- commands/mute.js
+++ commands/mute.js
@@ -1,4 +1,6 @@
 const { Command } = require('smooth-discord.js');
+const { findMember } = require('../util.js');
+const mutedId = '329522736927866880';
 
 module.exports = class LockdownCommand extends Command {
 	constructor() {
@@ -10,11 +12,22 @@ module.exports = class LockdownCommand extends Command {
 		});
 	}
 
-	run(message) {
-		if (message.member.roles.has('322794011133870080') || message.member.roles.has('292271962544275456')) {
-			message.channel.overwritePermissions(message.guild.roles.get(message.guild.id), { SEND_MESSAGES: false }).then(() => {
-				message.channel.send(`**${message.channel.name}** is now muted! Use \`-unmute\` to unmute the chat. `);
-			});
+	run(message, args) {
+		if (!args) {
+			if (message.member.roles.has('322794011133870080') || message.member.roles.has('292271962544275456')) {
+				message.channel.overwritePermissions(message.guild.roles.get(message.guild.id), { SEND_MESSAGES: false }).then(() => {
+					message.channel.send(`**${message.channel.name}** is now muted! Use \`-unmute\` to unmute the chat. `);
+				});
+			}
+		} else {
+			const argsArr = args.split(' ');
+			const reason = argsArr.slice(1).join(' ');
+			console.log(argsArr[0]);
+			const memberToMute = findMember(message, argsArr[0]);
+
+			if (!memberToMute) return message.reply('Could not find a user');
+			memberToMute.addRole(mutedId);
+			message.reply(`User **${memberToMute.displayName}** has been muted.\nReason: **${reason}**`);
 		}
 	}
 };
--- commands/unmute.js
+++ commands/unmute.js
@@ -1,4 +1,6 @@
 const { Command } = require('smooth-discord.js');
+const { findMember } = require('../util.js');
+const mutedId = '329522736927866880';
 
 module.exports = class LockdownCommand extends Command {
 	constructor() {
@@ -10,11 +12,21 @@ module.exports = class LockdownCommand extends Command {
 		});
 	}
 
-	run(message) {
-		if (message.member.roles.has('322794011133870080') || message.member.roles.has('292271962544275456')) {
-			message.channel.overwritePermissions(message.guild.roles.get(message.guild.id), { SEND_MESSAGES: null }).then(() => {
-				message.channel.send(`**${message.channel.name}** is now unmuted!`);
-			});
+	run(message, args) {
+		if (!args) {
+			if (message.member.roles.has('322794011133870080') || message.member.roles.has('292271962544275456')) {
+				message.channel.overwritePermissions(message.guild.roles.get(message.guild.id), { SEND_MESSAGES: null }).then(() => {
+					message.channel.send(`**${message.channel.name}** is now unmuted!`);
+				});
+			}
+		} else {
+			const argsArr = args.split(' ');
+			console.log(argsArr[0]);
+			const memberToMute = findMember(message, argsArr[0]);
+
+			if (!memberToMute) return message.reply('Could not find a user');
+			memberToMute.removeRole(mutedId);
+			message.reply(`User **${memberToMute.displayName}** has been unmuted`);
 		}
 	}
 };
